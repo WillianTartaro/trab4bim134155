137dc945fe02156bfb01c0e7619fd801484454ba
WillianTartaro <williantartaro@gmail.com>
2016-11-16 09:06:01 -0200

    PARTE 07 - Desenvolvido o upload de arquivos XML.
    Na pagina xhtml CadastroPessoas, foi criado uma nova aba responsavel por
    fazer o upload de arquivos XML, contendo informações de cadastros.
    
    ou seja, com o arquivo XML, contendo varias informações de contatos,
    podemos cadastrar uma sequencia de pessoas automaticamente.

--- AppJsf/src/main/java/br/com/tartaro/pessoa/controller/CadastrarPessoaController.java
+++ AppJsf/src/main/java/br/com/tartaro/pessoa/controller/CadastrarPessoaController.java
@@ -1,9 +1,21 @@
 package br.com.tartaro.pessoa.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
- 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import br.com.tartaro.model.PessoaModel;
 import br.com.tartaro.repository.PessoaRepository;
 import br.com.tartaro.usuario.controller.UsuarioController;
@@ -27,6 +39,17 @@ public class CadastrarPessoaController {
  
 	@Inject //pessoaRepository ira receber a injeção de dependencias.
 	PessoaRepository pessoaRepository;
+	
+	private UploadedFile file; //Variavel responsavel por agregar o xml.
+	 
+	/* ira capturar informações da variavel file.*/
+	public UploadedFile getFile() {
+		return file;
+	}
+ 
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
  
 	/* ira capturar informações do UsuarioModel.*/
 	public PessoaModel getPessoaModel() {
@@ -54,5 +77,78 @@ public class CadastrarPessoaController {
 		Uteis.MensagemInfo("Registro cadastrado com sucesso");
  
 	}
+
+	/**
+	 * Metodo responsavel por realizar o upload do arquivo XML, contendo as informações dos usuarios.
+	 */
+	 public void UploadRegistros() {
+ 
+		 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+ 
+		 try {
+ 
+ 
+			 if(this.file.getFileName().equals("")){
+				 Uteis.MensagemAtencao("Nenhum arquivo selecionado!");
+				 return;
+			 }
+ 
+			 DocumentBuilder builder = factory.newDocumentBuilder();
+ 
+	                 Document doc = builder.parse(this.file.getInputstream());
+ 
+	                 Element element = doc.getDocumentElement();
+ 
+	                 NodeList nodes = element.getChildNodes();
+ 
+	                for (int i = 0; i < nodes.getLength(); i++) {
+ 
+	        	     Node node  = nodes.item(i);
+ 
+	        	    if(node.getNodeType() == Node.ELEMENT_NODE){
+ 
+	        		 Element elementPessoa =(Element) node;
+ 
+	        		 //Pega as informações do XML.
+	        		 String nome     = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String sexo     = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String email    = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0).getNodeValue();
+ 
+	        		 PessoaModel newPessoaModel = new PessoaModel();
+ 
+	        		 //Faz o preenchimento dos campos necessarios para que o cadastro seja efetuado.
+	        		 newPessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+	        		 newPessoaModel.setEmail(email);
+	        		 newPessoaModel.setEndereco(endereco);
+	        		 newPessoaModel.setNome(nome);
+	        		 newPessoaModel.setOrigemCadastro("X");
+	        		 newPessoaModel.setSexo(sexo);
+ 
+	        		 //Salva em um novo registro, as informações vindo do XML.
+	        		 pessoaRepository.SalvarNovoRegistro(newPessoaModel);
+	        	   }
+	              }
+ 
+ 
+ 
+		     Uteis.MensagemInfo("Registros cadastrados com sucesso!");
+ 
+		     //Se caso houver erro, executa.
+		} catch (ParserConfigurationException e) {
+ 
+			e.printStackTrace();
+ 
+		} catch (SAXException e) {
+ 
+			e.printStackTrace();
+ 
+		} catch (IOException e) {
+ 
+			e.printStackTrace();
+		}
+ 
+ 
+	 }
  
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # trab4bim134155
 
-#Projeto com base no tutorial seguinte tutorial: http://www.ciceroednilson.com.br/criando-uma-aplicacao-java-com-jsf-primefaces-cdi-e-jpa-parte-1-introducao/
+*Projeto com base no tutorial seguinte tutorial: http://www.ciceroednilson.com.br/criando-uma-aplicacao-java-com-jsf-primefaces-cdi-e-jpa-parte-1-introducao/
 
-#Trabalho 4º Bimestre de Tópicos Avançados de programação Orientada a Objetos
+*Trabalho 4º Bimestre de Tópicos Avançados de programação Orientada a Objetos
